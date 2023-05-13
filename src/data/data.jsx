7a1913d7c07cd35af1c8a64d2ad2f688a8f365ef
Weldon <weldonprogrammer2022@gmail.com>
2023-05-13 03:37:56 -0300

    Criando array de objeto

---
+++ src/data/data.jsx
@@ -0,0 +1,81 @@
+import produto1 from "../assets/img-produtos/produto-01.jpeg"
+import produto2 from "../assets/img-produtos/produto-02.jpeg"
+import produto3 from "../assets/img-produtos/produto-03.jpeg"
+import produto4 from "../assets/img-produtos/produto-04.jpeg"
+import produto5 from "../assets/img-produtos/produto-05.jpeg"
+import produto6 from "../assets/img-produtos/produto-06.jpeg"
+import produto7 from "../assets/img-produtos/produto-07.jpeg"
+import produto8 from "../assets/img-produtos/produto-08.jpeg"
+
+const data = [
+    {
+        id:1,
+        nome: "AirPods Apple Fones de ouvido",
+        preco: 1.499,
+        descricao:"Em até 12x de R$ 124,92",
+        desconto:"R$ 1.349 à vista (10% de desconto)",
+        imgProduto: produto1
+    },
+    {
+        id:2,
+        nome: "Capa de silicone para iPhone 8/7 cor Areia - rosa",
+        preco: 299,
+        descricao:"Em até 12x de R$ 24,92",
+        desconto:"R$ 269,10 à vista (10% de desconto)",
+        imgProduto: produto2
+    },
+    {
+        id:3,
+        nome: "Apple Pencil",
+        preco: 729,
+        descricao:"Em até 12x de R$ 60,75",
+        desconto:"R$ 656,10 à vista (10% de desconto)",
+        imgProduto: produto3
+    },
+    {
+        id:4,
+        nome: "Magic Mouse 2 - Prateado",
+        preco: 549,
+        descricao:"Em até 12x de R$ 45,75",
+        desconto:"R$ 494,10 à vista (10% de desconto)",
+        imgProduto: produto4
+    },
+
+    {
+        id:5,
+        nome: "Caixa prateada de alumínio com pulseira esportiva branca",
+        preco: 2.899,
+        descricao:"Em até 12x de R$ 241,58",
+        desconto:"R$ 2.609,10 à vista (10% de desconto)",
+        imgProduto: produto5
+    },
+
+    {
+        id:6,
+        nome: "Cabo de lightning para USB (1m)",
+        preco: 149,
+        descricao:"Em até 12x de R$ 12,42",
+        desconto:"R$ 134,10 à vista (10% de desconto)",
+        imgProduto: produto6
+    },
+
+    {
+        id:7,
+        nome: "Smart Keyboard para iPad Pro 12,9 polegadas - inglês (EUA)",
+        preco: 1.099,
+        descricao:"Em até 12x de R$ 91,58",
+        desconto:"R$ 2.609,10 à vista (10% de desconto)",
+        imgProduto: produto7
+    },
+    {
+        id: 8,
+        nome: "Carregador USB de 5W Apple",
+        preco: 149,
+        descricao:"Em até 12x de R$ 12,42",
+        desconto:"R$ 134,10 à vista (10% de desconto)",
+        imgProduto: produto8
+    }
+
+]
+
+export default data;
