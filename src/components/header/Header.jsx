17ea17fbe2f13f5c196ea9d9e7b3be71b36f1bdb
Weldon <weldonprogrammer2022@gmail.com>
2023-05-13 03:34:02 -0300

    component topo

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+function Header() {
+  return (
+    <header className='cabecalho'>
+        
+       <h1 className='texto__header'>Produtos</h1> 
+
+    
+    </header>
+  )
+}
+
+export default Header
