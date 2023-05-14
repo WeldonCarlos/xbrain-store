6783c470acba6d577400edbeae3b73d69a57c9aa
Weldon <weldonprogrammer2022@gmail.com>
2023-05-14 04:16:03 -0300

    logica da função ativar modal por id

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,6 +4,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import {TextField}  from '@mui/material';
 import Typography from '@mui/material/Typography';
 import addIco from '../../assets/botoes/add.svg'
 import removeIco from '../../assets/botoes/remove.svg'
@@ -11,33 +12,96 @@ import './card.css'
 import data from '../../data/data';
 
 
+
 function Cartao() {
 
   const [dados, setDados] = useState(data);
-  const [ativar, setAtivar] = useState(false)
+  const [ativar, setAtivar] = useState(null)
+  const [incremento,setIncremento] = useState(1)
+  
+
+  //Função de ativar o modal por o id 
   function modalAdicionar(id) {
-    setAtivar(true)
+    setAtivar(id)
     console.log(id)
   }
+
+
+  //Funções de incremento e decremento dos produtos
+  function decremento(){
+    setIncremento(incremento-1)
+    if (incremento == 0 ){
+       return setIncremento(0)
+    }
+  }
+
+  function addIncremento(){
+    setIncremento(incremento+1)
+  }
+
+  //Função para fazer o dispatch para o redux
+  function adicionarProduto(){
+    alert(incremento)
+  }
+
+
 
   return (
     <main>
       <ul className='lista__itens'>
 
         {dados.map((item) => {
+          const isActive = item.id === ativar
+
           return (
             <li onClick={() => { modalAdicionar(item.id) }} className='itens' key={item.id}>
-              <Card sx={{ maxWidth: 240, height: '100%' }}>
+              <Card sx={{ maxWidth: 240, height: '96%'}}>
                 <CardMedia
                   sx={{ height: 230, }}
                   image={item.imgProduto}
                   title="green iguana"
                 />
-                <CardContent >
-                  <Typography sx={{ fontSize: ".8em", height: 35 }} gutterBottom variant="h5" component="div">
+                
+
+                  {isActive ? (
+                  <div className='active'>
+
+                  <CardContent id="conteiner-card" >
+                  <Typography id="titulo" gutterBottom variant="h5" component="div">
                     {item.nome}
                   </Typography>
-                  <Typography sx={{ fontSize: ".7em" }} variant="body2" color="text.secondary">
+                  <Typography id="preco" variant="body2" color="text.secondary">
+                   R$ {item.preco}
+
+                  </Typography>
+                  <Typography id="descricao" variant="body2" color="text.secondary">
+                    {item.descricao}
+
+                  </Typography>
+                  <Typography id="desconto" variant="body2" color="text.secondary">
+                    {item.desconto}
+
+                  </Typography> 
+                  
+                  </CardContent>
+
+                    <div className='decremento__incremento'>
+                      <button onClick={()=>{decremento()}}><img src={removeIco} alt="icone de sinal de menos" /></button>
+                      <TextField sx={{height:50}} type="Number" id='inputAdicionar' value={incremento} onChangeText={(text)=>{setIncremento(text)}}/>
+                      <button onClick={()=>{addIncremento()}}> <img src={addIco} alt="icone de sinal de mais" /></button>
+                    </div>
+
+                    <div className='container__adicionar'>
+                      <Button id='button-adicionar' onClick={()=>{adicionarProduto()}} variant="contained" >
+                        Adicionar
+                      </Button>
+                    </div>
+                  </div>):(
+                  <CardContent id="conteiner-card" >
+                  <Typography sx={{ fontSize: ".8em", height: 28 }} gutterBottom variant="h5" component="div">
+                    {item.nome}
+                  </Typography>
+                  <Typography id="preco" variant="body2" color="text.secondary">
                    R$ {item.preco}
 
                   </Typography>
@@ -48,23 +112,12 @@ function Cartao() {
                   <Typography sx={{ fontSize: ".7em" }} variant="body2" color="text.secondary">
                     {item.desconto}
 
-                  </Typography>
+                  </Typography> 
+                  
+                  </CardContent>
+                  )} 
 
-                  {ativar === true ? (<div className='active'>
-                    <div className='decremento__incremento'>
-                      <button><img src={removeIco} alt="icone de sinal de menos" /></button>
-                      <input type="number" value={1} />
-                      <button> <img src={addIco} alt="icone de sinal de mais" /></button>
-                    </div>
-
-                    <div className='container__adicionar'>
-                      <button className='button__adicionar'>
-                        ADICIONAR
-                      </button>
-                    </div>
-                  </div>) : (<div></div>)}
-
-                </CardContent>
+                
 
               </Card>
             </li>
