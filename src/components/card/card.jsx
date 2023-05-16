d37204c728658b98d53b07f3a94a4d9844fc067b
Weldon <weldonprogrammer2022@gmail.com>
2023-05-16 03:20:33 -0300

    refaturando o codigo do card e seu css

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import {TextField}  from '@mui/material';
+import { TextField } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import addIco from '../../assets/botoes/add.svg'
 import removeIco from '../../assets/botoes/remove.svg'
-import CardActions from '@mui/material/CardActions';
 import './card.css'
 import data from '../../data/data';
+
 
 
 
@@ -17,8 +16,8 @@ function Cartao() {
 
   const [dados, setDados] = useState(data);
   const [ativar, setAtivar] = useState(null)
-  const [incremento,setIncremento] = useState(1)
-  
+  const [incremento, setIncremento] = useState(1)
+
 
   //Função de ativar o modal por o id 
   function modalAdicionar(id) {
@@ -28,19 +27,19 @@ function Cartao() {
 
 
   //Funções de incremento e decremento dos produtos
-  function decremento(){
-    setIncremento(incremento-1)
-    if (incremento == 0 ){
-       return setIncremento(0)
+  function decremento() {
+    setIncremento(incremento - 1)
+    if (incremento == 0) {
+      return setIncremento(0)
     }
   }
 
-  function addIncremento(){
-    setIncremento(incremento+1)
+  function addIncremento() {
+    setIncremento(incremento + 1)
   }
 
   //Função para fazer o dispatch para o redux
-  function adicionarProduto(){
+  function adicionarProduto() {
     alert(incremento)
   }
 
@@ -55,69 +54,68 @@ function Cartao() {
 
           return (
             <li onClick={() => { modalAdicionar(item.id) }} className='itens' key={item.id}>
-              <Card sx={{ maxWidth: 240, height: '96%'}}>
-                <CardMedia
-                  sx={{ height: 230, }}
-                  image={item.imgProduto}
-                  title="green iguana"
-                />
-                
+              <Card id='cartoes' >
 
-                  {isActive ? (
+                <div id='container-img-cartao' >
+                  <img id='imagem-cartao' src={item.imgProduto} alt="imagem de um fone de ouvido da apple" />
+                </div>
+
+
+                {isActive ? (
                   <div className='active'>
 
-                  <CardContent id="conteiner-card" >
-                  <Typography id="titulo" gutterBottom variant="h5" component="div">
-                    {item.nome}
-                  </Typography>
-                  <Typography id="preco" variant="body2" color="text.secondary">
-                   R$ {item.preco}
+                    <CardContent id="conteiner-card" >
+                      <Typography id="titulo" gutterBottom variant="h5" component="div">
+                        {item.nome}
+                      </Typography>
+                      <Typography id="preco" variant="body2" color="text.secondary">
+                        R$ {item.preco}
 
-                  </Typography>
-                  <Typography id="descricao" variant="body2" color="text.secondary">
-                    {item.descricao}
+                      </Typography>
+                      <Typography id="descricao" variant="body2" color="text.secondary">
+                        {item.descricao}
 
-                  </Typography>
-                  <Typography id="desconto" variant="body2" color="text.secondary">
-                    {item.desconto}
+                      </Typography>
+                      <Typography id="desconto" variant="body2" color="text.secondary">
+                        {item.desconto}
 
-                  </Typography> 
-                  
-                  </CardContent>
+                      </Typography>
+
+                    </CardContent>
 
                     <div className='decremento__incremento'>
-                      <button onClick={()=>{decremento()}}><img src={removeIco} alt="icone de sinal de menos" /></button>
-                      <TextField sx={{height:50}} type="Number" id='inputAdicionar' value={incremento} onChangeText={(text)=>{setIncremento(text)}}/>
-                      <button onClick={()=>{addIncremento()}}> <img src={addIco} alt="icone de sinal de mais" /></button>
+                      <button onClick={() => { decremento() }}><img src={removeIco} alt="icone de sinal de menos" /></button>
+                      <TextField sx={{ height: 50 }} type="Number" id='inputAdicionar' value={incremento} onChangeText={(text) => { setIncremento(text) }} />
+                      <button onClick={() => { addIncremento() }}> <img src={addIco} alt="icone de sinal de mais" /></button>
                     </div>
 
                     <div className='container__adicionar'>
-                      <Button id='button-adicionar' onClick={()=>{adicionarProduto()}} variant="contained" >
+                      <Button id='button-adicionar' onClick={() => { adicionarProduto() }} variant="contained" >
                         Adicionar
                       </Button>
                     </div>
-                  </div>):(
+                  </div>) : (
                   <CardContent id="conteiner-card" >
-                  <Typography sx={{ fontSize: ".8em", height: 28 }} gutterBottom variant="h5" component="div">
-                    {item.nome}
-                  </Typography>
-                  <Typography id="preco" variant="body2" color="text.secondary">
-                   R$ {item.preco}
+                    <Typography sx={{ fontSize: ".8em", height: 28 }} gutterBottom variant="h5" component="div">
+                      {item.nome}
+                    </Typography>
+                    <Typography id="preco" variant="body2" color="text.secondary">
+                      R$ {item.preco}
 
-                  </Typography>
-                  <Typography sx={{ fontSize: ".7em" }} variant="body2" color="text.secondary">
-                    {item.descricao}
+                    </Typography>
+                    <Typography id="descri" variant="body2" color="text.secondary">
+                      {item.descricao}
 
-                  </Typography>
-                  <Typography sx={{ fontSize: ".7em" }} variant="body2" color="text.secondary">
-                    {item.desconto}
+                    </Typography>
+                    <Typography  id="descon" variant="body2" color="text.secondary">
+                      {item.desconto}
 
-                  </Typography> 
-                  
+                    </Typography>
+
                   </CardContent>
-                  )} 
+                )}
 
-                
+
 
               </Card>
             </li>
