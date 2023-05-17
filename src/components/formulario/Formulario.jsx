65a0a582698301e3109b3b9df255ab112682090f
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 02:09:07 -0300

    Organizando os imports do componente formulario por ordem alfabetica

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -1,85 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./style.css";
+import "./responsive.css"
 
-import './style.css'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Typography from '@mui/material/Typography';
-import BtnLaranja from '../botao-laranja/Btnlaranja';
+import Box from "@mui/material/Box";
+import BtnLaranja from "../botao-laranja/Btnlaranja";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+import { useSelector } from "react-redux";
 
 
 function Formulario() {
-    return (
-        <div>
+  const [nomeCliente, setNomeCliente] = useState("");
+  const [emailCliente, setEmailCliente] = useState("");
+  const [sexoCliente, setSexoCliente] = useState("");
 
-            <section className='cabecalho__dados__cliente'>
+  const preco = useSelector((state) => state.adicionar);
 
-                <h2 className='titulo__dados__cliente'>Dados do Cliente</h2>
+  return (
+    <div>
+      <section className="cabecalho__dados__cliente">
+        <h2 className="titulo__dados__cliente">Dados do Cliente</h2>
+      </section>
 
-            </section>
+      <Box id="form-container" component="form" noValidate autoComplete="off">
+        <TextField
+          id="inputs"
+          label="Nome"
+          placeholder="Nome do cliente aqui"
+          required
+          type="text"
+          value={nomeCliente}
+          onChange={(event) => {
+            setNomeCliente(event.target.value);
+          }}
+        />
+        <TextField
+          id="inputs"
+          type="email"
+          label="Email"
+          required
+          value={emailCliente}
+          placeholder="Digite seu email aqui"
+          onChange={(event) => {
+            setEmailCliente(event.target.value);
+          }}
+        />
 
-            <form onSubmit={()=>{}}>
-                <Box id='form-container'
-                    component="form"
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField
-                        error
-                        id="inputs"
-                        label="Nome"
-                        placeholder='Nome do cliente aqui'
-                        required
-                        value={name}
-                        onChange={(event) => {
-                            setName(event.target.value);
-                        }}
-                    />
-                    <TextField
-                        error
-                        id="inputs"
-                        label="Email"
-                        required
-                        placeholder='Digite seu email aqui'
-                        
-                    />
-
-                    <FormControl>
-                        <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="input-select"
-                            label="Sexo"
-
-
-                        >
-                            <MenuItem value={'Masculino'}>Masculino</MenuItem>
-                            <MenuItem value={'Feminino'}>Feminino</MenuItem>
-                            <MenuItem value={'LGBTQIAP+'}>LGBTQIAP+</MenuItem>
-                        </Select>
-
-                    </FormControl>
-
-                </Box>
-
-                <Box id='container-total'>
-                    <Typography id="total" gutterBottom variant="h5" component="div">
-                        Total: R$ 299,00
-                    </Typography>
-                   
-                   <BtnLaranja titulo='FINALIZAR COMPRA' rota='/finalizar'/>
-                </Box>
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
+          <Select
+            value={sexoCliente}
+            type="text"
+            labelId="demo-simple-select-label"
+            id="input-select"
+            label="Sexo"
+            onChange={(text) => setSexoCliente(text.target.value)}
+          >
+            <MenuItem value={"Masculino"}>Masculino</MenuItem>
+            <MenuItem value={"Feminino"}>Feminino</MenuItem>
+            <MenuItem value={"LGBTQIAP+"}>LGBTQIAP+</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
 
 
-            </form>
+      <Box id="container-total">
+        <Typography id="total" gutterBottom variant="h5" component="div">
+          {preco == 0
+            ? "Total: R$ 0,00"
+            : `Total: R$ ${preco}`}
+        </Typography>
 
-
-
-        </div>
-    )
+        <BtnLaranja
+          nomeCliente={nomeCliente}
+          titulo="FINALIZAR COMPRA"
+          rota="/finalizar"
+        />
+      </Box>
+    </div>
+  );
 }
 
-export default Formulario
+export default Formulario;
+
+
