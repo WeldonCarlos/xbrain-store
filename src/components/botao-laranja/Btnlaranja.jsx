26894c565305f3589ac88170a325f2d7a6d115c2
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 12:53:06 -0300

    refaturando codigo

--- src/components/botao-laranja/Btnlaranja.jsx
+++ src/components/botao-laranja/Btnlaranja.jsx
@@ -2,28 +2,17 @@ import React from 'react'
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './style.css'
 
 function BtnLaranja(props) {
-    const dispatch = useDispatch()
-   
-   //função para finalizar a compra e disparar uma action para o reducer no redux.
-    function finalizarCompra(){
-     const nomeCliente = props.nomeCliente;
-
-     
-     dispatch({
-      type: 'ADD_NOMECLIENTE',
-      nomeCliente
-      
-     })
-
-   }
+  
+  
 
   return (
     <div>
 
-      <Button onClick={()=> {finalizarCompra()}} type='submit' id='button-finalizar' variant="contained" >
+      <Button onClick={()=> {props.onClick()}} type='submit' id='button-finalizar' variant="contained" >
         <Link to={props.rota}>{props.titulo}</Link>
       </Button>
     </div>
