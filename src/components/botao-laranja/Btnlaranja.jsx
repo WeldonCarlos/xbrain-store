9e40326d5477f56bc737ff73772aa4b5df44d892
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 02:12:12 -0300

    Add  o Dispatch e a função finalizar compra ao componente Btnlaranja

--- src/components/botao-laranja/Btnlaranja.jsx
+++ src/components/botao-laranja/Btnlaranja.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './style.css'
 
 function BtnLaranja(props) {
+    const dispatch = useDispatch()
+   
+   //função para finalizar a compra e disparar uma action para o reducer no redux.
+    function finalizarCompra(){
+     const nomeCliente = props.nomeCliente;
+
+     
+     dispatch({
+      type: 'ADD_NOMECLIENTE',
+      nomeCliente
+      
+     })
+
+   }
+
   return (
     <div>
 
-      <Button type='submit' id='button-finalizar' variant="contained" >
-        <Link style={{fontSize:props.fonte}} to={props.rota}>{props.titulo}</Link>
+      <Button onClick={()=> {finalizarCompra()}} type='submit' id='button-finalizar' variant="contained" >
+        <Link to={props.rota}>{props.titulo}</Link>
       </Button>
     </div>
   )
