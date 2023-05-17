d17148cf40ceddaa11632ad6aef8f92c9a5d7ae2
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 02:06:34 -0300

    Organização dos imports por ordem alfabetica

--- src/pages/finalizar-compra/Finalizar.jsx
+++ src/pages/finalizar-compra/Finalizar.jsx
@@ -1,35 +1,58 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import BtnLaranja from '../../components/botao-laranja/Btnlaranja';
-import likebuy from '../../assets/finalizar/like.png'
-import './style.css'
-function Finalizar() {
-  return (
-    <section className='conteiner-geral'>
-      <Card  id='container-card-finalizar'>
+import React, { useState } from "react";
+import "./style.css";
 
+import BtnLaranja from "../../components/botao-laranja/Btnlaranja";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import likebuy from "../../assets/finalizar/like.png";
+import Typography from "@mui/material/Typography";
+
+
+import { useSelector } from "react-redux";
+import numeral from 'numeral'
+
+
+function Finalizar() {
+
+  const nomeCliente = useSelector((state) => state.nomeCliente);
+  const preco = useSelector((state) => state.adicionar)
+
+
+
+  return (
+    <section className="conteiner-geral">
+      <Card id="container-card-finalizar">
         <CardContent>
-          <Typography id='nome-card-finalizar' gutterBottom variant="h5" component="div">
-            John Doe,
+          <Typography
+            id="nome-card-finalizar"
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {nomeCliente}
           </Typography>
-          <Typography id='descricao-card-finalizar' variant="body2" color="text.secondary">
-            Sua compra no valor <span className='valor__finalizar' >R$ 299,00</span> foi finalizada com sucesso
+          <Typography
+            id="descricao-card-finalizar"
+            variant="body2"
+            color="text.secondary"
+          >
+            Sua compra no valor{" "}
+            <span className="valor__finalizar">R$ {preco.toLocaleString({ style: 'currency', currency: 'BRL' })}</span> foi finalizada
+            com sucesso
           </Typography>
-          <div  className='container-img-like' >
-            <img className='like' src={likebuy} alt="imagem de uma mãozinho fazendo sinal de like" />
+          <div className="container-img-like">
+            <img
+              className="like"
+              src={likebuy}
+              alt="imagem de uma mãozinho fazendo sinal de like"
+            />
           </div>
         </CardContent>
-        <BtnLaranja fonte={12} titulo='INICIAR NOVA COMPRA' rota='/'  />
+        <BtnLaranja fonte={12} titulo="INICIAR NOVA COMPRA" rota="/" />
       </Card>
-
-
     </section>
-  )
+  );
 }
 
-export default Finalizar
+
+export default Finalizar;
