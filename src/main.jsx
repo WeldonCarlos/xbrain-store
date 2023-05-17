f4e12eb3ed891e1830357d13d23d7148431e291c
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 02:12:40 -0300

    Add as rotas e provedor do Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import{ createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/home/Home.jsx';
+import Finalizar from './pages/finalizar-compra/Finalizar.jsx';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
-import{ createBrowserRouter, RouterProvider } from "react-router-dom"
 
-import Home from './pages/home/home.jsx'
-import Finalizar from './pages/finalizar-compra/finalizar.jsx'
 const router = createBrowserRouter([
 
   {
@@ -26,6 +27,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+    <Provider store={store}>
+     <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>
 )
