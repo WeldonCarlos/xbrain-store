e72002b85b97fa6187c69e8e0538df699b270b12
Weldon <weldonprogrammer2022@gmail.com>
2023-05-17 12:49:58 -0300

    refaturando a função

--- src/pages/finalizar-compra/Finalizar.jsx
+++ src/pages/finalizar-compra/Finalizar.jsx
@@ -6,18 +6,28 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import likebuy from "../../assets/finalizar/like.png";
 import Typography from "@mui/material/Typography";
+import { useDispatch } from 'react-redux';
 
 
 import { useSelector } from "react-redux";
-import numeral from 'numeral'
+
 
 
 function Finalizar() {
 
   const nomeCliente = useSelector((state) => state.nomeCliente);
   const preco = useSelector((state) => state.adicionar)
-
-
+  
+  const dispatch = useDispatch();
+ 
+  const iniciarNovaCompra = () => {
+    alert("botao funcionando")
+    dispatch({
+      type:"LIMPAR",
+      preco 
+    }); 
+  };
+  
 
   return (
     <section className="conteiner-geral">
@@ -37,7 +47,7 @@ function Finalizar() {
             color="text.secondary"
           >
             Sua compra no valor{" "}
-            <span className="valor__finalizar">R$ {preco.toLocaleString({ style: 'currency', currency: 'BRL' })}</span> foi finalizada
+            <span className="valor__finalizar">{`R$ ${preco.toLocaleString({ style: 'currency', currency: 'BRL' })}.00`}</span> foi finalizada
             com sucesso
           </Typography>
           <div className="container-img-like">
@@ -48,7 +58,12 @@ function Finalizar() {
             />
           </div>
         </CardContent>
-        <BtnLaranja fonte={12} titulo="INICIAR NOVA COMPRA" rota="/" />
+        <BtnLaranja 
+         fonte={12}
+         titulo="INICIAR NOVA COMPRA"
+         rota="/"
+         onClick={iniciarNovaCompra}
+         />
       </Card>
     </section>
   );
